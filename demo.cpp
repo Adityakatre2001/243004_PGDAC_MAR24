8a98c7bdd55790b3129951cb374c6df74ff1efc7
MonikaGahane <monu.gahane1997@gmail.com>
2024-04-26 16:39:29 +0530

    Commit by Aditya

---
+++ demo.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+  int a;
+  /*cout<<"enter a value" << endl;
+  cin>>a;
+  iostream>
+using namespace std;
+int main1()  {
+    char ch ;i
+	cout<<"Enter a character"<<endl;
+	cin>>ch;
+	cout<<sizeof(ch);
+	return 0;  
+   
+   
+  return 0;
+  
+}
+
